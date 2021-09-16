aad01173a36a53be54f643cae0b0fe9ff1703996
RicardoMCode <ricardomaldonado1204@gmail.com>
2021-09-16 00:11:14 -0500

    Se completa la integración entr el HTML y el Js, se crean los input que permiten ingresar los datos desde UI retornando, por ahora, en un alert los resultados

--- figuras.js
+++ figuras.js
@@ -13,4 +13,32 @@ const diametroCirculo = (radio) => radio * 2;
 const perimetroCirculo = (radio) => diametroCirculo(radio) * Math.PI;
 const areaCirculo = (radio) => radio**2 *Math.PI;
 
-
+//Onclicks 
+function calcularPerimetroCuadrado(){
+    const TbxLado = document.getElementById("lado");
+    alert(perimetroCuadrado(TbxLado.value));
+}
+function calcularAreaCuadrado(){
+    const TbxLado = document.getElementById("lado");
+    alert(areaCuadrado(TbxLado.value));
+}
+function calcularPerimetroTriangulo (){
+    const TbxLado1 = document.getElementById("lado1");
+    const TbxLado2 = document.getElementById("lado2");
+    const TbxLado3 = document.getElementById("lado3");
+    alert(perimetroTriangulo(Number(TbxLado1.value),Number(TbxLado2.value),Number(TbxLado3.value)));
+}
+function calcularAreaTriangulo (){
+    const TbxLado1 = document.getElementById("lado1");
+    const TbxLado2 = document.getElementById("lado2");
+    const TbxLado3 = document.getElementById("lado3");
+    alert(areaTriangulo(Number(TbxLado1.value),Number(TbxLado2.value),Number(TbxLado3.value)));
+}
+function calcularPerimetroCirculo (){
+    const TbxRadio = document.getElementById("radio");
+    alert(perimetroCirculo(radio.value));
+}
+function calcularAreaCirculo (){
+    const TbxRadio = document.getElementById("radio");
+    alert(areaCirculo(radio.value));
+}
